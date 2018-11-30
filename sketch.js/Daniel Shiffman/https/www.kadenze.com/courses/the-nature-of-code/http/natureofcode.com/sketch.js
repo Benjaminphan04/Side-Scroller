c8fcf6ea870dcb91e721d5282aa78e3d7ba9c175
Benjaminphan04 <bphan1@ocdsb.ca>
2018-11-30 14:00:49 -0500

    Update sketch.js

--- sketch.js/Daniel Shiffman/https/www.kadenze.com/courses/the-nature-of-code/http/natureofcode.com/sketch.js
+++ sketch.js/Daniel Shiffman/https/www.kadenze.com/courses/the-nature-of-code/http/natureofcode.com/sketch.js
@@ -1,18 +1,27 @@
-// Daniel Shiffman
-// https://www.kadenze.com/courses/the-nature-of-code
-// http://natureofcode.com/
-// Session 2: Array of Particles, multiple forces
-
 var person;
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(720, 450);
 	person = new Person();
 }
 
+function keyPressed
+
 function draw() {
-  background(51);
-    person.update();
-	  person.display();
+  background(100);
 	
+	if(mouseIsPressed){
+		
+	var force = createVector( -0.001,0);
+      person.applyForce(force);
+	}
+  
+	translate( -person.pos.x , 0 );	
+	person.update();
+    person.display();
+
+	
+// immobile obstacle
+  fill(235);
+  rect (620,385,100,65);
 }
