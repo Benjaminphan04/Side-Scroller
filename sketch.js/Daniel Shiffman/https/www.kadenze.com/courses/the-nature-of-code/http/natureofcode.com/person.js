1c543eb0ae03cfef97ffd853d17705cdde20d7d0
Benjaminphan04 <bphan1@ocdsb.ca>
2018-12-03 15:01:56 -0500

    Update and rename particle.js to person.js

--- sketch.js/Daniel Shiffman/https/www.kadenze.com/courses/the-nature-of-code/http/natureofcode.com/person.js
+++ sketch.js/Daniel Shiffman/https/www.kadenze.com/courses/the-nature-of-code/http/natureofcode.com/person.js
@@ -1,13 +1,14 @@
 function Person() {
-  this.pos = createVector(10, height);
-  this.vel = createVector(5, 0);
+  this.pos = createVector(50, height);
+  this.vel = createVector(1, 0);
   this.acc = createVector(0, 0);
+
 
   this.applyForce = function(force) {
     this.acc.add(force);
   }
 
-  this.update = function() {
+    this.update = function() {
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.set(0, 0);
@@ -16,18 +17,13 @@ function Person() {
   this.display = function() {
     fill(25,29,30);
     stroke(255);
-	  rect(this.pos.x, this.pos.y-50, 50, 45);
+	  rect(this.pos.x, this.pos.y-50, 20, 50);
   }
 
   this.edges = function() {
     if (this.pos.y > height) {
-      this.vel.y *= -1;
+      this.vel.y *= 0;
       this.pos.y = height;
-    }
-
-    if (this.pos.x > width) {
-      this.vel.x *= -1;
-      this.pos.x = width;
-    }
+    }  
   }
 }
